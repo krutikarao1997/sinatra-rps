862bfb7bc9e2cdc95573c6fe13e287624e023171
krutikarao1997 <krutikarao@chicagobooth.edu>
2025-04-15 04:28:52 +0000

    game

--- app.rb
+++ app.rb
@@ -2,8 +2,51 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "Tied"
+  elsif @comp_move == "paper"
+    @outcome = "Lost"
+  else 
+    @outcome = "Won"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "Won"
+  elsif @comp_move == "paper"
+    @outcome = "Tied"
+  else 
+    @outcome = "Lost"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "Tied"
+  elsif @comp_move == "paper"
+    @outcome = "Won"
+  else 
+    @outcome = "Tied"
+  end
+
+  erb(:scissors)
+end
+
